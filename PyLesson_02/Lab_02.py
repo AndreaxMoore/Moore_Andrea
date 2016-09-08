26d8965f79c2c26a4eace066af5807b08f6360b7
AndreaxMoore <MooreA8957@sduhsd.lan>
2016-09-08 09:45:14 -0700

    Added files

---
+++ PyLesson_02/Lab_02.py
@@ -0,0 +1,25 @@
+a = 3
+b = 4
+ab = 12
+print(a,"multiplied by",b,"is",ab)
+
+name = "Bill & Aileen M."
+address = "975 Bennett Drive"
+city = "Longwood, "
+zipcode = "FL 32750"
+print(name)
+print(address)
+print(city+zipcode)
+
+l= 5
+h = 6
+w = 2
+lh = l*h
+lw = l*w
+hw = h*w
+surfaceArea =(lh+lw+hw+lh+lw+hw)
+print("The surface area of your rectangle is", surfaceArea)
+
+
+
+
