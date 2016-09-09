5d78dc02159092e7f079804e1d1072c88aee2078
AndreaxMoore <MooreA8957@sduhsd.lan>
2016-09-09 10:26:30 -0700

    Added files

---
+++ PyLesson_03/cylinderVolume.py
@@ -0,0 +1,5 @@
+radius = int(input("What is the radius of your cylinder?"))
+height = int(input("What is the height of your cylinder?"))
+pi = 3.14
+volume = pi*radius**2*height
+print("The volume of our cylinder is",volume)
