5a3f177db3101cca42167b2b69398c5620ea43d7
AndreaxMoore <MooreA8957@sduhsd.lan>
2016-09-21 09:51:03 -0700

    Changed BMI.py

--- PyLesson_03/BMI.py
+++ PyLesson_03/BMI.py
@@ -1,5 +1,5 @@
 height = int(input("What is your height in inches?"))
 weight = int(input("What is your weight in pounds?"))
 num = 703
-BMI = 703*weight/height**2
+BMI = num*weight/height**2
 print("Your BMI is",BMI)
