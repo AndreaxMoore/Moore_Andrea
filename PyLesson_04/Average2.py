7896f1c435903efe950e07081a8a8af19fe388cc
AndreaxMoore <MooreA8957@sduhsd.lan>
2016-10-03 10:15:26 -0700

    Changed files

--- PyLesson_04/Average2.py
+++ PyLesson_04/Average2.py
@@ -1,6 +1,6 @@
 def average(n,nu,num):
     global mean
-    mean = float((num1+num2+num3)/3)
+    mean = float((n+nu+num)/3)
 def display(ave):
     print("The average of",num1,",",num2,",",num3,",is","{:0.5f}".format(ave),".")
 num1 = float(input("Please enter the first number: "))
--- PyLesson_04/Cube2.py
+++ PyLesson_04/Cube2.py
@@ -1,6 +1,6 @@
-def calcSurf(side):
+def calcSurf(s):
     global area
-    area = float(6*(side*side))
+    area = float(6*(s*s))
 def display(are):
     print("The surface area of a cube whose sides are",side,"in length is","{:0.5f}".format(are),".")
 side = float(input("Please enter the length of the sides: "))
