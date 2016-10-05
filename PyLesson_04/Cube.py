1d9becf7a58d545c46b7e09cf010c3bbb936cec1
AndreaxMoore <MooreA8957@sduhsd.lan>
2016-10-05 09:33:24 -0700

    Changed Files

--- PyLesson_04/Cube.py
+++ PyLesson_04/Cube.py
@@ -2,6 +2,6 @@
 def calcSurf(s):
     return(6*(s*s))
 def display(s):
-    print("The surface area of a cube whose sides are",s,"in length is","{:0.5f}".format(calcSurf(s)),".")
-side = float(input("Please enter the length of the sides: "))
+    print("The surface area of a cube whose sides are",s,"in length is","{:0.5f}".format(calc
+                                                                                         side = float(input("Please enter the length of the sides: "))
 display(side)
---
+++ PyLesson_04/Return.py
@@ -0,0 +1,8 @@
+def method1(num):
+    return num
+
+def numPrint(num):
+    print("The number is",method(num))
+
+num1 = 4
+numPrint(num1)
