6ffc13c90c44cd9e1d04c73075c710fb0c84b2e3
AndreaxMoore <andreaxmoore@gmail.com>
2016-10-06 10:20:16 -0700

    Changed Files

--- PyLesson_04/Rectangle.py
+++ PyLesson_04/Rectangle.py
@@ -2,7 +2,7 @@
 def calcPerim(le,wi):
     return(2*(le+wi))
 def display(le,wi):
-    print("Your rectangle is","{:0.5f}".format(calcPerim(le,wi)),"sq ft around.")
+    print("Your rectangle is","{:0.5f}".format(calcPerim(le,wi)),"ft around.")
 length = float(input("Please enter the length of your rectangle: "))
 width = float(input("Please enter the width of your rectangle: "))
 
