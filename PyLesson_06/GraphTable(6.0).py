4c5dd67d2ee8825b33819c235e2aed1cc896c2ff
AndreaxMoore <andreaxmoore@gmail.com>
2016-10-28 10:20:38 -0700

    Changed Files

--- PyLesson_06/GraphTable(6.0).py
+++ PyLesson_06/GraphTable(6.0).py
@@ -1,7 +1,8 @@
 size = int(input("Please enter the size of the table: "))
+integer = int(input("please enter an integer: "))
 
-def printgraph():
-    for i in range(1,size+1):
-        print(i,(4*i+1))
-printgraph()
+for i in range(1,size+1):
+    print(i,(i*integer))
+
+
 
