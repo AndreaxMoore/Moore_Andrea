ba95860b1bbb4380d1eba9709d95b5b4496944c0
AndreaxMoore <andreaxmoore@gmail.com>
2016-10-28 10:07:07 -0700

    Changed files

--- PyLesson_04/Subwoofer Boxes(4.1).py
+++ PyLesson_04/Subwoofer Boxes(4.1).py
@@ -4,5 +4,5 @@ h = float(input("Please enter the height in inches: "))
 l = float(input("Please enter the length in inches: "))
 w = float(input("Please enter the width in inches: "))
 
-print("The volume of the subwoofer box is:","{:5.2f}".format(volume(h,l,w)),"cubic feet") 
+print("The volume of the subwoofer box is:{:5.2f}".format(volume(h,l,w)),"cubic feet") 
 
