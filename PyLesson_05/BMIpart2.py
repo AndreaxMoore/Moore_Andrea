c24f6c670f68c88f21a3c3533449afca7cc25dea
AndreaxMoore <andreaxmoore@gmail.com>
2016-11-02 12:22:52 -0700

    Changed Files

--- PyLesson_05/BMIpart2.py
+++ PyLesson_05/BMIpart2.py
@@ -1,19 +1,22 @@
 height = float(input("Enter your height in inches: "))
 weight = float(input("Enter your weight in pounds: "))
-BMI = 703*(weight/(height**2))
+BMI = 0
 
 def calcBMI():
+    global condition, BMI
+    BMI = 703*(weight/(height**2))
     if BMI < 18.5:
-        return "Underweight"
+        condition = "Underweight"
     elif BMI < 24.9:
-        return "Normal"
+        condition = "Normal"
     elif BMI < 29.9:
-        return "Overweight"
+        condition = "Overweight"
     elif BMI < 34.9:
-        return "Obese"
+        condition = "Obese"
     elif BMI < 39.9:
-        return "Very Obese"
+        condition = "Very Obese"
     else:
-        return "Morbidly Obese"
+        condition = "Morbidly Obese"
+calcBMI()
 print("Your BMI is:",BMI)
-print("You are", calcBMI(),".")
+print("You are", condition,".")
