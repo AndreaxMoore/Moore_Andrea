fd3390b64fa9e70e959848feb119586437914b1f
AndreaxMoore <andreaxmoore@gmail.com>
2016-11-04 09:58:59 -0700

    Changed Files

--- PyLesson_06/CountByX(6.1).py
+++ PyLesson_06/CountByX(6.1).py
@@ -1,6 +1,6 @@
-num1 = int(input("Please enter a number1: "))
-num2 = int(input("Please enter a number2: "))
+num1 = int(input("Please enter the number you want to count to: "))
+num2 = int(input("Please enter the number you want to count by: "))
 output = ""
-for i in range(num1,num2+1,2):
+for i in range(num2,num1+1,num2):
     output = output + str(i)+ " "
 print(output)
--- PyLesson_06/GraphTable(6.0).py
+++ PyLesson_06/GraphTable(6.0).py
@@ -1,8 +1,7 @@
 size = int(input("Please enter the size of the table: "))
 integer = int(input("please enter an integer: "))
 
-for i in range(1,size+1):
-    print(i,(i*integer))
 
-
+for i in range(1, size+1):
+    print("{:<5}{:<5}".format(i,i*integer))
 
