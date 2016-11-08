eaed44130e4a1a43465da26e5f4adf5f1747a752
AndreaxMoore <andreaxmoore@gmail.com>
2016-11-08 09:40:01 -0800

    Changed Files

--- PyLesson_08/Underscores.py
+++ PyLesson_08/Underscores.py
@@ -1,8 +1,8 @@
 sentence = input("Please enter a sentence: ")
 
 def replace(sentence):
-    
-    if sentence.count(" ") <= 0:
+   
+    if sentence.count(" ") == 0:
         return sentence
     else:
         return replace(sentence[0:sentence.index(" ")]+"_"+sentence[sentence.index(" ")+1:len(sentence)])
