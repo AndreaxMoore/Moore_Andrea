2926c0b26b67e320ed2afaca9fc9f59bc417455c
AndreaxMoore <andreaxmoore@gmail.com>
2016-11-09 09:57:06 -0800

    Added Files

---
+++ PyLesson_08/TreeDeg60.py
@@ -0,0 +1,13 @@
+word = input("Please enter a word: ")
+stop = len(word)
+start = 0
+
+def tree(word,start,stop):
+    if start <= stop:
+        print("{:>8}".format(word[0:start]))
+        start += 1
+        tree(word,start,stop)
+
+tree(word,start,stop)
+
+
