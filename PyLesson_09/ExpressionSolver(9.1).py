904629c7eee88133c5c6a64c2b27b71e1d753bda
AndreaxMoore <MooreA8957@sduhsd.lan>
2016-11-29 10:18:55 -0800

    Changed Files

---
+++ PyLesson_09/ExpressionSolver(9.1).py
@@ -0,0 +1,27 @@
+expression = input("Please enter a mathematical expression: ")
+equation = expression.split(" ")
+
+i = 0
+while i < len(equation):
+    if i < len(equation) and equation[i]== ("*" or "/"):
+        if equation[i] == "*":
+            equation.append(equation[i-1]*(i+1))
+        else:
+            equation.append(equation[i-1]/(i+1))
+        equation.remove(equation[i-1])
+        equation.remove(equation[i])
+    i += 1
+
+while i in equation < len(equation):
+    if i < len(equation) and equation[i] == ("+" or "-"):
+        if equation[i] == "+":
+            equation.append(equation[i-1]+(i+1))
+        else:
+            equation.append(equation[i-1]-(i+1))
+        equation.remove(equation[i-1])
+        equation.remove(equation[i])
+    i += 1
+print(equation)
+        
+        
+    
