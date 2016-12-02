7473088e3804b79404ddab611d9fd5eb9719ec14
AndreaxMoore <MooreA8957@sduhsd.lan>
2016-12-02 10:08:43 -0800

    Changed Files

--- PyLesson_09/ExpressionSolver(9.1).py
+++ PyLesson_09/ExpressionSolver(9.1).py
@@ -3,25 +3,26 @@ equation = expression.split(" ")
 
 i = 0
 while i < len(equation):
-    if i < len(equation) and equation[i]== ("*" or "/"):
+    if i < len(equation) and (equation[i]== "*" or equation[i] == "/"):
         if equation[i] == "*":
-            equation.append(equation[i-1]*(i+1))
+            equation[i] = int(equation[i-1])*int(equation[i+1])
         else:
-            equation.append(equation[i-1]/(i+1))
+            equation[i] = int(equation[i-1])/int(equation[i+1])
         equation.remove(equation[i-1])
         equation.remove(equation[i])
     i += 1
 
-
-    if i < len(equation) and equation[i] == ("+" or "-"):
+i = 0
+while i < len(equation):
+    if i < len(equation) and (equation[i] == "+" or equation[i] == "-"):
         if equation[i] == "+":
-            equation.append(equation[i-1]+(i+1))
+            equation[i] = int(equation[i-1])+int(equation[i+1])
         else:
-            equation.append(equation[i-1]-(i+1))
+            equation[i] = int(equation[i-1])-int(equation[i+1])
         equation.remove(equation[i-1])
         equation.remove(equation[i])
     i += 1
 print(equation)
         
         
-    
+
