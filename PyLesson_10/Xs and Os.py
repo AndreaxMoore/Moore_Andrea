dfb29e6eada6384069f02886e9941656d93a3a37
AndreaxMoore <MooreA8957@sduhsd.lan>
2016-12-16 10:05:19 -0800

    Changed Files

--- PyLesson_10/Xs and Os.py
+++ PyLesson_10/Xs and Os.py
@@ -1,16 +1,16 @@
 xAndO = []
+import random
 for i in range(0,4):
     xAndO.append([])
     for j in range(0,4):
-        import random
         switch = random.randint(0,1)
         if switch == 1:
-            xAndO[i].append(["X"])
+            xAndO[i].append("X")
         else:
-            xAndO[i].append(["O"])
+            xAndO[i].append("O")
 
 for values in xAndO:
     output = ""
     for value in values:
-        output += str(value)
+        output += value
     print(output)
