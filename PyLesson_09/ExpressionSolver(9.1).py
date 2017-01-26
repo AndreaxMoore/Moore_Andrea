e4f5c064478245366e80d9cdddf6d19e448b0b85
AndreaxMoore <MooreA8957@sduhsd.lan>
2017-01-26 10:28:58 -0800

    Changed Files

--- PyLesson_09/ExpressionSolver(9.1).py
+++ PyLesson_09/ExpressionSolver(9.1).py
@@ -8,10 +8,9 @@ while i < len(equation):
             equation[i] = int(equation[i-1])*int(equation[i+1])
         else:
             equation[i] = int(equation[i-1])/int(equation[i+1])
-        equation.remove(equation[i-1])
-        equation.remove(equation[i])
+        equation.pop(i-1)
+        equation.pop(i)
     i += 1
-
 i = 0
 while i < len(equation):
     if i < len(equation) and (equation[i] == "+" or equation[i] == "-"):
@@ -19,10 +18,10 @@ while i < len(equation):
             equation[i] = int(equation[i-1])+int(equation[i+1])
         else:
             equation[i] = int(equation[i-1])-int(equation[i+1])
-        equation.remove(equation[i-1])
-        equation.remove(equation[i])
+        equation.pop(i-1)
+        equation.pop(i)
     i += 1
 print(equation)
         
-        
+
 
